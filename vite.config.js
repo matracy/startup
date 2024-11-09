092c2940d23c7e6b2c89b40106ba72e822063531
marcus <marcus.a.tracy@gmail.com>
2024-11-08 17:19:49 -0700

    Fixed config errors for Vite and npm

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    istanbul({
-      include: ['src/**/*'],
-      exclude: ['node_modules'],
-      requireEnv: false,
-    }),
-  ],
+	plugins: [react()],
 });
