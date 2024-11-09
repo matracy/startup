c6a8cd2d0ed9593a0581908e6c9225e3c54e608d
marcus <marcus.a.tracy@gmail.com>
2024-11-08 21:15:45 -0700

    First version of Vite project that actually builds

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
 	const [count, setCount] = useState(0);
@@ -10,13 +7,10 @@ function App() {
 		<>
 			<div>
 				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
+					<img src="/logo-square.svg" className="logo" alt="STVOnline logo" />
 				</a>
 			</div>
-			<h1>Marcus Tracy</h1>
+			<h1>STV Online</h1>
 			<div className="card">
 				<button onClick={() => setCount((count) => count + 10)}>
 					count is {count}
@@ -25,9 +19,8 @@ function App() {
 					Edit <code>src/App.jsx</code> and save to test HMR
 				</p>
 			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
 		</>
 	);
 }
+
+export { App };
