e4d218de971e4ea062536d5c68f380c73abc035e
marcus <marcus.a.tracy@gmail.com>
2024-11-16 16:42:15 -0700

    Updated frontend to use api rather than mocked responses

--- src/backendInterface.js
+++ src/backendInterface.js
@@ -1,45 +1,92 @@
+const baseURL = "https://stvonline.someplaceto.click/api";
+
 function registerNewUser(credentials, notifyCallback) {
-	notifyCallback({ name: credentials.username });
+	const response = fetch(`${baseURL}/auth`, {
+		method: "POST",
+		headers: { credentials: credentials },
+	});
+	const rjson = response.json;
+	notifyCallback({ name: rjson.name, token: rjson.token });
 }
 
-function registerPoll(opts, config) {
-	//This will only need to talk to the server
+function registerPoll(opts, config, authToken, notifyCallback) {
+	const response = fetch(`${baseURL}/poll/create`, {
+		method: "POST",
+		body: JSON.stringify({ opttions: opts, settings: config }),
+		headers: { authToken: authToken },
+	});
+	const rjson = response.json;
+	notifyCallback({ pollID: newPoll, registrationNumber: registrationNumber });
 }
 
-function fetchOptions() {
-	return [
-		{ name: "Option 1", rank: 0 },
-		{ name: "Option 2", rank: 0 },
-		{ name: "Option 3", rank: 0 },
-	];
+function fetchOptions(pollID) {
+	const response = fetch(`${baseURL}/poll`, {
+		method: "GET",
+		headers: { pollID: pollID },
+	});
+	var parsedOptions = [];
+	response.json.options.forEach((opt) => {
+		parsedOptions.push({ name: opt.name, rank: 0 });
+	});
+	return parsedOptions;
 }
 
-function castVote(ballot) {
-	//This will only need to talk to the server
+function castVote(pollID, ballot, authToken) {
+	var formattedBallot = {};
+	ballot.forEach((opt) => {
+		formattedBallot[opt] = opt.rank;
+	});
+	const response = fetch(`${baseURL}/poll`, {
+		method: "PATCH",
+		body: JSON.stringify(formattedBallot),
+		headers: { pollID: pollID, authToken: authToken },
+	});
+	const rjson = response.json;
 }
 
-function fetchResults(poll) {
-	return [
-		{ name: "Option 1", initialVotes: 1, finalVotes: 2 },
-		{ name: "Option 2", initialVotes: 1, finalVotes: 1 },
-		{ name: "Option 3", initialVotes: 1, finalVotes: 0 },
-	];
+function fetchResults(pollID) {
+	const response = fetch(`${baseURL}/poll`, {
+		method: "GET",
+		headers: { pollID: pollID },
+	});
+	return response.json.options;
 }
 
 function authenticateUser(credentials, notifyCallback) {
-	notifyCallback({ name: credentials.username });
+	const response = fetch(`${baseURL}/auth`, {
+		method: "GET",
+		headers: { credentials: credentials },
+	});
+	const rjson = response.json;
+	notifyCallback({ name: rjson.name, token: rjson.token });
 }
 
-function registerToVote() {
-	//This will only need to talk to the server
+function registerToVote(registrationNumber, authToken, notifyCallback) {
+	const response = fetch(`${baseURL}/poll/register`, {
+		method: "POST",
+		headers: { authToken: authToken },
+		body: JSON.stringify({ registrationNumber: registrationNumber }),
+	});
+	notifyCallback({ pollID: response.json.pollID });
 }
 
-function fetchPolls(user) {
-	return [
-		{ name: "Poll A", result: "Option 1" },
-		{ name: "Poll B", result: "Option 2" },
-		{ name: "Poll C", result: "Option 3" },
-	];
+function fetchPolls(authToken) {
+	const response = fetch(`${baseURL}/user`, {
+		method: "GET",
+		headers: { authToken: authToken },
+	});
+	var pollInfo = [];
+	response.json.forEach((pollID) => {
+		const pollResults = fetch(`${baseURL}/poll`, {
+			method: "GET",
+			headers: { pollID: pollID },
+		});
+		pollInfo.push({
+			name: pollResults.json.id,
+			result: pollResults.json.result,
+		});
+	});
+	return pollInfo;
 }
 
 export {
