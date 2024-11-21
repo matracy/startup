8fb1b0496e27584415d6ed0d5a4652069552db5f
marcus <marcus.a.tracy@gmail.com>
2024-11-20 23:53:48 -0700

    User handler updated to use new signatures from authentication and poll services

--- service/userHandler.js
+++ service/userHandler.js
@@ -3,13 +3,19 @@ import { getPollsVotedIn } from "./pollServices.js";
 
 function userHandler(req, res) {
 	const token = req.headers["authtoken"];
-	console.log(`Got authToken ${token}`);
-	console.log(Object.keys(req.headers));
 	if (!token || !validateToken(token)) {
 		return res.status(401).json({ message: "invalid credentials" });
 	}
-	const user = getUser(token);
-	res.status(200).json(getPollsVotedIn(user));
+	validateToken(token, (isValid) => {
+		if (!isValid) {
+			return res.status(401).json({ message: "invalid credentials" });
+		}
+		getUser(token, (user) => {
+			getPollsVotedIn(user, (polls) => {
+				res.status(200).json(polls);
+			});
+		});
+	});
 }
 
 export { userHandler };
