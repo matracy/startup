483399786791b3c9e6888cfa4cf8b548a1a31a70
marcus <marcus.a.tracy@gmail.com>
2024-11-21 00:09:43 -0700

    Poll router now uses new signatures for authentication and polling services

--- service/pollRouter.js
+++ service/pollRouter.js
@@ -19,95 +19,131 @@ pollRouter.get("/", (req, res) => {
 	if (!pollID) {
 		return res.status(400).json({ message: "pollID is required" });
 	}
-	const poll = lookupPoll(pollID);
-	if (!poll) {
-		return res.status(404).json({ message: "no poll with matching pollID" });
-	}
-	res
-		.status(200)
-		.json({ id: pollID, options: poll.options, result: poll.result });
+	const poll = lookupPoll(pollID, (poll) => {
+		if (!poll) {
+			return res.status(404).json({ message: "no poll with matching pollID" });
+		}
+		res
+			.status(200)
+			.json({ id: pollID, options: poll.options, result: poll.result });
+	});
 });
 
 // Cast vote
 pollRouter.patch("/", (req, res) => {
 	const token = req.headers["authtoken"];
-	if (!token || !validateToken(token)) {
+	if (!token) {
 		return res.status(401).json({ message: "invalid credentials" });
 	}
 	const pollID = req.headers["pollid"];
 	if (!pollID) {
 		return res.status(400).json({ message: "pollID is required" });
 	}
-	const poll = lookupPoll(pollID);
-	if (!poll) {
-		return res.status(404).json({ message: "no poll with matching pollID" });
-	}
-	const ballot = req.body;
-	if (!ballot) {
-		return res.status(400).json({ message: "ballot is required" });
-	}
-	var pollOptions = Object.keys(poll.options);
-	var ballotOptions = Object.keys(ballot);
-	var votedOnAllIssues = pollOptions.reduce((acc, opt) => {
-		return acc && opt in ballotOptions;
-	}, true);
-	if (!votedOnAllIssues) {
-		return res.status(400).json({ message: "must vote on all options" });
-	}
-	//now that validation is done, actually cast the vote
-	if (countBallot(ballot, pollID, getUser(token))) {
-		res.status(202).send();
-	} else {
-		return res.status(500).json({ message: "Error processing ballot." });
-	}
+	validateToken(token, (isValid) => {
+		if (isValid) {
+			lookupPoll(pollID, (poll) => {
+				if (!poll) {
+					return res
+						.status(404)
+						.json({ message: "no poll with matching pollID" });
+				}
+				const ballot = req.body;
+				if (!ballot) {
+					return res.status(400).json({ message: "ballot is required" });
+				}
+				var pollOptions = Object.keys(poll.options);
+				var ballotOptions = Object.keys(ballot);
+				var votedOnAllIssues = pollOptions.reduce((acc, opt) => {
+					return acc && opt in ballotOptions;
+				}, true);
+				if (!votedOnAllIssues) {
+					return res.status(400).json({ message: "must vote on all options" });
+				}
+				//now that validation is done, actually cast the vote
+				getUser(token, (user) => {
+					countBallot(ballot, pollID, user, (wasCounted) => {
+						if (wasCounted) {
+							res.status(202).send();
+						} else {
+							return res
+								.status(500)
+								.json({ message: "Error processing ballot." });
+						}
+					});
+				});
+			});
+		} else {
+			return res.status(401).json({ message: "invalid credentials" });
+		}
+	});
 });
 
 // Register to vote
 pollRouter.post("/register", (req, res) => {
 	const token = req.headers["authtoken"];
-	if (!token || !validateToken(token)) {
+	if (!token) {
 		return res.status(401).json({ message: "invalid credentials" });
 	}
 	const pollID = req.headers["pollid"];
 	if (!pollID) {
 		return res.status(400).json({ message: "pollID is required" });
 	}
-	const poll = lookupPoll(pollID);
-	if (!poll) {
-		return res.status(404).json({ message: "no poll with matching pollID" });
-	}
-	const { registrationNumber } = req.body;
-	if (!registrationNumber) {
-		return res.status(400).json({ message: "registration number is required" });
-	}
-	const unlockedPoll = registerToVote(registrationNumber);
-	if (!unlockedPoll) {
-		return res.status(400).json({ message: "invalid registration number" });
-	} else {
-		res.status(200).json({ pollID: unlockedPoll });
-	}
+	validateToken(token, (isValid) => {
+		if (isValid) {
+			lookupPoll(pollID, (poll) => {
+				if (!poll) {
+					return res
+						.status(404)
+						.json({ message: "no poll with matching pollID" });
+				}
+				const { registrationNumber } = req.body;
+				if (!registrationNumber) {
+					return res
+						.status(400)
+						.json({ message: "registration number is required" });
+				}
+				registerToVote(registrationNumber, (unlockedPoll) => {
+					if (!unlockedPoll) {
+						return res
+							.status(400)
+							.json({ message: "invalid registration number" });
+					} else {
+						res.status(200).json({ pollID: unlockedPoll });
+					}
+				});
+			});
+		} else {
+			return res.status(401).json({ message: "invalid credentials" });
+		}
+	});
 });
 
 // Create poll
 pollRouter.post("/create", (req, res) => {
 	const token = req.headers["authtoken"];
-	if (!token || !validateToken(token)) {
+	if (!token) {
 		return res.status(401).json({ message: "invalid credentials" });
 	}
-	const { options, settings } = req.body;
-	if (!options || !settings) {
-		return res
-			.status(400)
-			.json({ message: "options and settings are required" });
-	}
-	const { newPoll, registrationNumber } = createPoll(options, settings);
-	if (!newPoll || !registrationNumber) {
-		return res.status(500).json({ message: "could not create new poll" });
-	} else {
-		res
-			.status(200)
-			.json({ pollID: newPoll, registrationNumber: registrationNumber });
-	}
+	validateToken(token, (isValid) => {
+		if (isValid) {
+			const { options, settings } = req.body;
+			if (!options || !settings) {
+				return res
+					.status(400)
+					.json({ message: "options and settings are required" });
+			}
+			const { newPoll, registrationNumber } = createPoll(options, settings);
+			if (!newPoll || !registrationNumber) {
+				return res.status(500).json({ message: "could not create new poll" });
+			} else {
+				res
+					.status(200)
+					.json({ pollID: newPoll, registrationNumber: registrationNumber });
+			}
+		} else {
+			return res.status(401).json({ message: "invalid credentials" });
+		}
+	});
 });
 
 export { pollRouter };
