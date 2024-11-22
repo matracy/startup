30728f72da0d9db6d0f966cde89bcfea95915b29
marcus <marcus.a.tracy@gmail.com>
2024-11-21 23:44:00 -0700

    API requests now succeed and are correctly parsed

--- service/colorMagic.js
+++ service/colorMagic.js
@@ -1,6 +1,6 @@
 import { writeFile } from "fs";
 
-const interval = 60 * 1000; // refresh every minute
+const interval = 2 * 60 * 1000; // refresh every two minutes (timing of imitation curl command showed 1.6 minute wait)
 const filename = "dynamic-colors.css";
 const fileRelPath = "./public/"; //NOTE: must end in '/'
 
@@ -10,19 +10,19 @@ const fileWarningHeader = `/*
 * Any changes made here will be overwritten.
 */`;
 
-function convertRGBListToHexString(lst) {
-	var res = "";
-	lst.forEach((channel) => {
-		res += channel.toStrning(16);
-	});
-	return res;
-}
-
-function convertColorListToStringList(lst) {
+function convertColorResponseToStringList(colorResponse) {
 	var res = [];
-	lst.forEach((color) => {
-		res.push(convertRGBListToHexString(color));
+	var hexPairs = [];
+	Object.keys(colorResponse).forEach((key) => {
+		const color = colorResponse[key];
+		color.forEach((chanel) => {
+			hexPairs.push(chanel.toString(16));
+		});
 	});
+	for (let pos = 0; pos < hexPairs.length / 3; pos++) {
+		let start = pos * 3;
+		res.push([hexPairs[start] + hexPairs[start + 1] + hexPairs[start + 2]]);
+	}
 	return res;
 }
 
@@ -31,19 +31,21 @@ async function regenerateColors() {
 		const response = await fetch("http://colormind.io/api/", {
 			method: "POST",
 			headers: {
+				"User-Agent": " curl/8.5.0",
+				Accept: "*/*",
 				"Content-Type": "application/x-www-form-urlencoded",
 			},
 			body: '{"model":"ui"}',
 		});
 		//example result:
 		// {"result":[[246,247,244],[145,119,90],[115,153,163],[107,84,80],[62,66,75]]}
+		// For some reason, the code sees the above as 246, 247, 244, 145, 119, 90, 115, 153, 163, 107, 84, 80, 62, 66, 75
 
 		if (!response.ok) {
 			throw new Error(`Response status: ${response.status}`);
 		}
-		const colors = await response.json()["result"];
-		console.log(`got ${colors} from colormind.`);
-		const colorStrings = convertColorListToStringList(colors);
+		const colors = await response.json();
+		const colorStrings = convertColorResponseToStringList(colors.result);
 
 		const body = `:root {
 			--primaryColor: #${colorStrings[0]};
