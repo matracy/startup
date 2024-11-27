043573377cc09609505c702d025d166ef12dbcbb
marcus <marcus.a.tracy@gmail.com>
2024-11-26 22:45:09 -0700

    Ballot counting now actually records the results

--- service/STVMagic.js
+++ service/STVMagic.js
@@ -11,24 +11,16 @@ function sortByValue(obj) {
 function countVotes(ballots, validOptions) {
 	//FIXME why does this make votes[opt] undefined?
 	var votes = {};
-	console.log(
-		`Valid options for this counting cycle are ${JSON.stringify(validOptions)}`,
-	);
 	ballots.forEach((ballot) => {
-		console.log(`Filtering ballot ${JSON.stringify(ballot)}`);
 		const filteredOpts = ballot.filter((opt) => {
 			return validOptions.includes(opt);
 		});
-		console.log(`Filtered options are ${JSON.stringify(filteredOpts)}`);
 		const preferredOption = filteredOpts[0];
 		if (votes[preferredOption]) {
 			votes[preferredOption] += 1;
 		} else {
-			votes[preferredOption] = 0;
+			votes[preferredOption] = 1;
 		}
-		console.log(
-			`Set votes for ${preferredOption} to ${votes[preferredOption]}`,
-		);
 	});
 	return votes;
 }
@@ -43,20 +35,18 @@ function updatePoll(poll) {
 		sortedBallots.push(sortByValue(ballot));
 	});
 	//NOTE: sortByValue orders least to greatest, so the *least* popular candidate will be first in the resulting array.
-	const initialVotes = countVotes(sortedBallots, options);
+	var initialVotes = {};
+	initialVotes = countVotes(sortedBallots, options);
 	var finalVotes = initialVotes;
 	while (Object.keys(finalVotes).length > 2) {
 		finalVotes = countVotes(sortedBallots, sortByValue(finalVotes).slice(1));
 	}
-	options.forEach((opt) => {
-		console.log(
-			`Setting [${opt}] to have ${initialVotes[opt]} initial votes and ${finalVotes[opt]} final votes`,
-		);
-		poll.options[opt].initialVotes = initialVotes[opt] ? initialVotes[opt] : 0;
-		poll.options[opt].finalVotes = finalVotes[opt] ? finalVotes[opt] : 0;
+	poll.options.map((opt) => {
+		opt.initialVotes = initialVotes[opt.name] ? initialVotes[opt.name] : 0;
+		opt.finalVotes = finalVotes[opt.name] ? finalVotes[opt.name] : 0;
 	});
 	poll.result = sortByValue(finalVotes)[1]; //NOTE: least popular candidates are sorted sooner
-	console.log(`Final state of poll: ${JSON.stringify(poll)}`);
+	return poll;
 }
 
 export { updatePoll };
